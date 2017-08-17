0cb42283265272c07f9ee9c306477b9ee0a8623b
k3nsalazar2012 <k3nsalazar2012@gmail.com>
2017-08-17 14:34:14 -0700

    First commit from local machine

---
+++ Angry Birds/Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 4f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+        Debug.Log("died");
+    }
+}
---
+++ Angry Birds/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+    private bool isPressed = false;
+    public float maxDragDistance = 2f;
+    public float releaseTime = 0.15f;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+            else
+                rb.position = mousePos;
+        }
+    }
+
+    void OnMouseDown()
+    { 
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+    }
+    
+}
